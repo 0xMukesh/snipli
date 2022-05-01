1b93a1549f8528483aa604d06a787b36c187e2f3
Kira272921 <sunset272921@gmail.com>
2022-05-01 22:00:18 +0530

    :sparkles: feat: support for gist url in edit cmd

--- src/commands/edit.ts
+++ src/commands/edit.ts
@@ -20,17 +20,26 @@ export default class Edit extends Command {
     id: Flags.string({
       char: "i",
       description: "ID of the Gist which you are going to edit",
-      required: true,
+      multiple: false,
+      exclusive: ["url"],
+    }),
+    url: Flags.string({
+      char: "u",
+      description: "URL of the Gist which you are going to edit",
+      exclusive: ["id"],
+      multiple: false,
     }),
     file: Flags.string({
       char: "f",
       description: "The path of the file which has the edited content",
       required: true,
+      multiple: false,
     }),
     description: Flags.string({
       char: "d",
       description: "The description of the Gist",
       required: true,
+      multiple: false,
     }),
     public: Flags.boolean({
       char: "p",
@@ -64,13 +73,18 @@ export default class Edit extends Command {
     const spinner = ora("Updating gist...").start();
 
     try {
+      let gistId = flags.id;
+
+      if (flags.url) {
+        gistId = flags.url.split("/").pop();
+      }
+
       await updateGist(
-        // @ts-ignore
         getFileName(flags.file) as string,
         fs.readFileSync(flags.file as string, "utf8"),
         flags.description as string,
         flags.public,
-        flags.id as string
+        gistId as string
       );
       spinner.succeed("Updated the gist successfully!");
     } catch (err) {
