ae3b6e68b34abba41bf755d86a3961c3ec1ccc6f
Kira272921 <sunset272921@gmail.com>
2022-05-01 12:08:34 +0530

    :pencil2: fix: change the done page url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,6 +2,6 @@ import os from "os";
 
 const tokenConfigFileLocation = `${os.homedir()}/.snipli/config.json`;
 const apiUrl = "https://snipli-production.up.railway.app";
-const authSuccessRedirectUrl = "https://snipli.vercel.app/done";
+const authSuccessRedirectUrl = "https://snipli.xyz/done";
 
 export { tokenConfigFileLocation, apiUrl, authSuccessRedirectUrl };
