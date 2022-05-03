145f47de2cea62ca9cae111712f10e95b96f234b
Kira272921 <sunset272921@gmail.com>
2022-05-03 10:39:36 +0530

    :sparkles: feat: add manual token input flag

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,17 +1,33 @@
-import { Command } from "@oclif/core";
+import { Command, Flags } from "@oclif/core";
 
 import chalk from "chalk";
 
 import authorizeViaCli from "../lib/authorizeViaCli";
+
 import getToken from "../utils/getToken";
+import tokenConfig from "../utils/tokenConfig";
 
 export default class Login extends Command {
   static description = "🔑 Login into snipli via GitHub";
 
   static examples = ["snipli login"];
 
+  static flags = {
+    token: Flags.string({
+      char: "t",
+      description: "Manually use personal access token to authenticate",
+      required: false,
+    }),
+  };
+
   async run() {
+    const { flags } = await this.parse(Login);
+
     if (getToken() === null) {
+      if (flags.token !== undefined) {
+        tokenConfig(flags.token as string);
+        return;
+      }
       await authorizeViaCli();
     } else {
       console.log(chalk.yellow("\nYou're already logged in!"));
